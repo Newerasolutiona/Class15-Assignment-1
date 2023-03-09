d631f3c7940130a079ad01b9ce59b6b3dbaeba80
Karelle Matchum <karelledacheu@gmail.com>
2023-03-09 06:20:41 +0000

    Added if statement

--- Karelle/bash/even_and_odd_num.sh
+++ Karelle/bash/even_and_odd_num.sh
@@ -2,6 +2,13 @@
 
 # script that identifies even and odd numbers
 
-for
+for num in {1..50}
+
 do
+	if (($num % 2 == 0))
+        then
+                echo "$num is even"
+        else
+                echo "$num is odd"
+        fi
 done
